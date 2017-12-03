f62a8b3019702abf582064904f9052bf64c41257
vikevstgn <nika9kukusha@gmail.com>
2017-12-03 13:29:01 +0300

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Second commit");
         System.out.println("Grustno");
+        System.out.println("Sovsem grustno");
     }
 }
